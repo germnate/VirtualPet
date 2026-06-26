3acbf64cb9f7ac52daf3f002b90b807bbe64a178
nathan <bustamanten2@gmail.com>
2026-06-25 20:00:48 -0400

    allow local host for react

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,     // exposes to LAN (0.0.0.0)
+    port: 5173      // optional, but keeps it consistent
+  }
 })
